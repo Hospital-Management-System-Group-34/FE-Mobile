0c1c56d4a28478d028b9f903ffcb2528dd13caa8
Zxniaav <wahyukurnia1751@gmail.com>
2022-06-03 13:21:20 +0700

    update data pasien screen

--- hospital_management_system/lib/view/screens/datapasien_screen.dart
+++ hospital_management_system/lib/view/screens/datapasien_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hospital_management_system/widgets/pasien_card.dart';
 
 import '../../widgets/poppins_text.dart';
 
@@ -23,28 +24,73 @@ class _DataPasienState extends State<DataPasien> {
         ),
       ),
       body: Container(
+        padding: const EdgeInsets.all(20),
         child: Column(
           children : [
-            // SizedBox(
-            //           child: TextFormField(
-            //             key: Key('search'),
-            //             style: const TextStyle(
-            //               fontSize: 16,
-            //             ),
-            //             decoration: InputDecoration(
-            //               contentPadding:
-            //                   const EdgeInsets.symmetric(vertical: 4.0),
-            //               prefixIcon: const Icon(Icons.search_rounded),
-            //               hintStyle: PoppinsText.blackBold().styleText(16),
-            //               hintText: "Search",
-            //               filled: true,
-            //               isDense: true,
-            //               border: OutlineInputBorder(
-            //                 borderRadius: BorderRadius.circular(10),
-            //               ),
-            //             ),
-            //           ),
-            //         ),
+            SizedBox(
+                      child: TextFormField(
+                        key: Key('search'),
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                        decoration: InputDecoration(
+                          contentPadding:
+                              const EdgeInsets.symmetric(vertical: 4.0),
+                          prefixIcon: const Icon(Icons.search_rounded),
+                          hintText: "Search",
+                          filled: true,
+                          isDense: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+            SizedBox(height: 15),
+            Container(
+              height: 400,
+              width: double.infinity,
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Container(
+                  height: 110,
+                  width: 330,
+                  margin: const EdgeInsets.only(bottom: 20),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blue, width: 2),
+                      borderRadius: BorderRadius.circular(5.0)),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 10,
+                        left: 18,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            PoppinsText.blue('Nama Pasien', 12),
+                            PoppinsText.blackSemiBold('Alteeza Akbar', 14),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            PoppinsText.blue('No. Peserta', 12),
+                            PoppinsText.blackSemiBold('1098765432', 14),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        right: 10,
+                        child: myPopUpMenu(),
+                      ),
+                      
+                    ],
+                  ),
+                );
+                },
+              ),
+              ),
+
 
           ]
         )
