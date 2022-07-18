960c1a4c1fa866593f3346fac468d672fd2b8f6c
Zxniaav <wahyukurnia1751@gmail.com>
2022-07-18 22:40:13 +0700

    transition update

---
+++ hospital_management_system/lib/widgets/transition.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+PageTransition transition({required PageTransitionType type, required Widget page}) {
+  return PageTransition(
+    type: type, 
+    duration: const Duration(milliseconds: 500),
+    child: page
+  ); 
+}
